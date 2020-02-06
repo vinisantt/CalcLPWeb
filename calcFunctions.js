88cf5b92e335d958725982c9c0d7c2be2c738b74
Vinicius Dias <vdias6849@gmail.com>
2020-02-06 19:25:36 -0300

    funcoes

---
+++ calcFunctions.js
@@ -0,0 +1,15 @@
+function soma(a, b) {
+  return a + b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+function multiplica(a, b) {
+  return a * b;
+}
+
+function subtrai(a, b) {
+  return a - b;
+}
